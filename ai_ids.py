2f933141823a77a8b2e170a2e95a6e7b00d70553
MOHD EHSAN MUZAMMIL <ehsaan1151@gmail.com>
2026-04-16 22:28:46 +0530

    Update ai_ids.py
    CODE

--- ai_ids.py
+++ ai_ids.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 # AI
-from sklearn.ensemble import IsolationForest
+from sklearn.ensemble import IsolationForest 
 import numpy as np
 
 # ---------------- GLOBAL VARIABLES ----------------
